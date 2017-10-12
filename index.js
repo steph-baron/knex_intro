3ea3de01025ef8143664f52a520fcad42f0ea5f1
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-12 14:47:23 -0400

    updated javascript

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const env = 'development'
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
+
+// knex('car').then(function(result){
+//   console.log(result);
+//   knex.destroy();
+// })
+// .catch(function (err) {
+//   console.log(err);
+//   process.exit(1);
+// });
+
+console.log("Show me a car that runs");
+
+knex('car')
+.where('model', 'Civic')
+.then(function(result){
+  console.log(result);
+  knex.destroy();
+})
+.catch(function (err) {
+  console.log(err);
+  process.exit(1);
+});
